32af0a62c02a896beffce2d254aee0442ceb91aa
adarchez <amarodarchez@gmail.com>
2022-03-24 20:47:57 -0300

    commit2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { AuthProvider } from "./context/AuthContext";
+import RouterHandle from "./components/RouterHandle";
+import Header from "./pages/Header";
+import Footer from "./pages/Footer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthProvider>
+        <Header></Header>
+        <RouterHandle></RouterHandle>
+        <Footer></Footer>
+      </AuthProvider>
     </div>
   );
 }
